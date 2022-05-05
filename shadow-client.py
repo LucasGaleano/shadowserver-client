5f497c54cb7c6989a8f1d4828d068086c7c530e0
lucasgaleano <l.a.galeano2@gmail.com>
2022-05-05 18:20:48 -0300

    added description changes

--- shadow-client.py
+++ shadow-client.py
@@ -15,6 +15,29 @@ uri = "https://transform.shadowserver.org/api2/"
 
 TIMEOUT = 45
 
+reportDescription = {
+    'blocklist':'IP Blocklist',
+    'device_id':'Device Identification',
+    'event4_honeypot_brute_force':'Brute-force attack observed by honeypots',
+    'event4_sinkhole':'Sinkhole connections',
+    'event4_sinkhole_http':'Sinkhole connections to HTTP',
+    'scan6_telnet':'Open IPv6 telnet found',
+    'scan_ftp':'Open FTP found',
+    'scan_ipmi':'Open IPMI found',
+    'scan_isakmp':'Open Isakmp found',
+    'scan_mdns':'Open mDNS found',
+    'scan_netbios':'Open netbios found',
+    'scan_portmapper':'Open portmapper found',
+    'scan_rdp':'open RDP found',
+    'scan_rsync':'Open rsync found',
+    'scan_smb':'Open SMB found',
+    'scan_snmp':'Open SNMP found',
+    'scan_ssl_poodle':'Open SSLv3 found',
+    'scan_telnet':'Open telnet found',
+    'scan_tftp':'Open TFTP found'
+}
+
+
 
 def add_ip_port_fields(device):
     '''
@@ -58,11 +81,11 @@ def modify_description(reportTypes):
     Add description to netbios and ntp. Also add consistency to the other description.
     '''
     for reportType in reportTypes:
-        if reportType['type'] == 'scan_netbios':
-            reportType['description'] = "Open Netbios Scan"
-        elif reportType['type'] == 'scan_ntp':
-            reportType['description'] = "Open NTP Scan"
-        reportType['description'] = reportType['description'].replace("Results","").replace("Scan","Scanned").replace("Scannedned","Scanned").strip()
+        try:
+            reportType['description'] = reportDescription[reportType['type']]
+        except:
+            print("report with no description")
+
     return reportTypes
 
 def log_json(jsonFile, content, output=True):
@@ -103,6 +126,7 @@ if __name__ == '__main__':
         lastDateReport = reports[-1]['timestamp'] # Getting date of the more update report.
 
         reportTypes = json.loads(api_call("reports/types", json.loads(f'{{"detail":true, "date":"{lastDateReport}"}}')))
+        
         reportTypes = modify_description(reportTypes)
 
         with open('log.json','a') as jsonFile:
